e1711387bbafe7178be9d4970945d686057dfc02
Hendrik Müller <hendrik.mueller@devboost.de>
2018-06-19 11:58:55 +0200

    Adding links to sidebar and project headline

--- projects/views.py
+++ projects/views.py
@@ -2,16 +2,30 @@ from django.shortcuts import render
 import requests
 
 
+def fetch_repos():
+    r = requests.get('https://api.github.com/users/henne90gen')
+    data = r.json()
+    r = requests.get(data['repos_url'])
+    return r.json()
+
+
+def fetch_project(project_name: str):
+    r = requests.get(f'https://api.github.com/repos/henne90gen/{project_name}')
+    data = r.json()
+    return data
+
+
 def index(request):
     """
     Homepage
     """
-    r = requests.get('https://api.github.com/users/henne90gen')
-    data = r.json()
-    for key in data:
-        print(key, data[key])
-    r = requests.get(data['repos_url'])
-    repos = r.json()
-    for repo in repos:
-        print(repo)
-    return render(request, 'index.html')
+    context = {'repos': fetch_repos()}
+    return render(request, 'index.html', context=context)
+
+
+def project(request, name: str):
+    context = {
+        'repos': fetch_repos(),
+        'project': fetch_project(name)
+    }
+    return render(request, 'project.html', context=context)
